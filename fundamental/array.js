58a5f34c6620980319b34cb01510616e6cc7320e
indrajeet790 <indrajeetkumarnayak3@gmail.com>
2023-09-21 22:31:52 +0530

    array methods

--- fundamental/array.js
+++ fundamental/array.js
@@ -1,7 +1,7 @@
-const array = [1,2,3,4,5];
-const myHeros = ["ironman","Thor"]
+// const array = [1,2,3,4,5];
+// const myHeros = ["ironman","Thor"]
 
-const arr2 = new Array(1,2,3,4)
+// const arr2 = new Array(1,2,3,4)
 // console.log(array[1]);
 
 // Array METHOD
@@ -13,9 +13,39 @@ const arr2 = new Array(1,2,3,4)
 
 
 // shift and unshift
-array.unshift(7)
-array.shift()
-console.log(array);
+// array.unshift(7)
+// array.shift()
+// console.log(array);
 
+const marvel_heros= ["thor","ironman","captain"]
+const dc_heros=["sktiman","sonpari","black_panther"]
+
+// push method using
+// marvel_heros.push(dc_heros);
+// console.log(marvel_heros);
+
+// concat method
+// const AllHeros=marvel_heros.concat(dc_heros)
+// console.log(AllHeros)
+
+// spread oprator
+const all_new_Heros=[...marvel_heros,...dc_heros]
+console.log(all_new_Heros);
+
+
+// const another_array= [1,2,3, [4,5,6],7,[6,7,[4,5]]]
+// const real_another_array=another_array.flat(Infinity)
+// console.log(real_another_array)
+
+// 
+console.log(Array.isArray("INDRAJEET"))
+console.log(Array.from("Indrajeet"))
+
+//  multiple value can bind into a single array
+let score1 =200
+let score2=300
+let score3 =400
+
+console.log(Array.of(score1,score2,score3))
 
 
