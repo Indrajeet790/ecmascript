04f226fd0c0fa1e21ebdbfa41db31fd412006527
indrajeet790 <indrajeetkumarnayak3@gmail.com>
2023-09-21 22:12:10 +0530

    fundamental array in javacript

---
+++ fundamental/array.js
@@ -0,0 +1,21 @@
+const array = [1,2,3,4,5];
+const myHeros = ["ironman","Thor"]
+
+const arr2 = new Array(1,2,3,4)
+// console.log(array[1]);
+
+// Array METHOD
+// array.push(6);
+// array.push(8);
+// array.pop()
+// console.log(array);
+
+
+
+// shift and unshift
+array.unshift(7)
+array.shift()
+console.log(array);
+
+
+
