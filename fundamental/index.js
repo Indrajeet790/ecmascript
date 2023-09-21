3525acb97a978547939baa64a27010b57804b2b9
indrajeet790 <indrajeetkumarnayak3@gmail.com>
2023-09-21 08:57:05 +0530

    let const and var varibale

---
+++ fundamental/index.js
@@ -0,0 +1,7 @@
+// let const and var 
+const accountId=12345654
+let accountEmail="abc123@gmail.com"
+let accountPassword="12345"
+let accountCity="jaipur"
+
+console.table([accountId,accountEmail,accountPassword,accountCity]);
