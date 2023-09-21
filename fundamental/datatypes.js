d5727f34c772916ee9b7ebd847149ccabbd471d6
indrajeet790 <indrajeetkumarnayak3@gmail.com>
2023-09-21 09:15:36 +0530

    datatype

---
+++ fundamental/datatypes.js
@@ -0,0 +1,10 @@
+// Number=>2 to power 53,
+// String=>"",
+// BigInt,
+// Boolean =>true/false
+// null =>standAlone value
+// NaN, 
+// undefined,
+// symbole=>unique,
+// object
+console.log(typeof null);
--- fundamental/index.js
+++ fundamental/index.js
@@ -1,7 +1,10 @@
 // let const and var 
+/*
 const accountId=12345654
 let accountEmail="abc123@gmail.com"
 let accountPassword="12345"
 let accountCity="jaipur"
 
 console.table([accountId,accountEmail,accountPassword,accountCity]);
+*/
+
