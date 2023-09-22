81de2f9616070bced869488d6062620a5efce9dd
indrajeet790 <indrajeetkumarnayak3@gmail.com>
2023-09-22 21:59:41 +0530

    function passed as object,array , parameter

--- fundamental/function.js
+++ fundamental/function.js
@@ -1,5 +1,6 @@
 1.// function declaration
 // paramerter
+/*
 function twoNumber(num1, num2) {
     return num1 + num2;
 
@@ -21,3 +22,49 @@ function loginUserMesssage(username = "gupta") {
 }
 const user = loginUserMesssage()
 console.log(user)
+*/
+
+
+// ++++++++++++++++++++++++++++++++++++++++++++
+
+// rest operator
+function calculateCartPrice(valu1,valu2,...num1){
+    return num1
+
+}
+console.log(calculateCartPrice(200,300,400,500));
+
+
+
+
+// ++++++++++++++++++++
+// A object passed into a function
+const user = {
+    username:"indrajeet",
+    prices:199,
+}
+
+function handleObject(user){
+    console.log(`username is ${user.username} and price is ${user.price}`);
+    
+
+}
+// handleObject(user)
+handleObject({
+    username:"vijay",
+    price:299
+})
+
+
+// array paased into function
+const myNewArray=[200,300,400,500]
+
+function returnSecondValue(getArray){
+    return getArray[2]
+}
+// console.log(returnSecondValue(myNewArray))
+console.log(returnSecondValue([200,300,400,500]))
+
+
+
+
