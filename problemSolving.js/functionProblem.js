1429a541297cb2c3242a75020d170837251a5d86
indrajeet790 <indrajeetkumarnayak3@gmail.com>
2023-09-23 14:50:34 +0530

    catetalize first letter of word

--- problemSolving.js/functionProblem.js
+++ problemSolving.js/functionProblem.js
@@ -26,10 +26,30 @@ console.log(reverseNumber(123456))
 
 
 // problem 7:check wheter a passed string is palindrom or not
-
+/*
 function stringPalChecker(str){
     var reverse = str.split("").reverse().join("");
     if(reverse === str) return true;
     else return false;
 }
 console.log(stringPalChecker("151"))
+*/
+
+// problem 8: write a javascript function that return a passed string with letters in alphaetical order.
+
+function stringAlphaBeticalOrder(str){
+    return str.split("").sort().join("");
+}
+console.log(stringAlphaBeticalOrder("apple"))
+
+// problem 9: capetilize first letter of word
+
+function capitalize(str){
+    var allWords = str.split(" ").map(function(word){
+       return word.charAt(0).toUpperCase()+ word.substring(1)
+    })
+    return allWords.join(" ");
+    
+
+}
+console.log(capitalize("kya jaan kaisi ho"));
