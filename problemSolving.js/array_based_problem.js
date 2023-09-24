e6b32670352eab3149c8ef468be3b247d80275fd
indrajeet790 <indrajeetkumarnayak3@gmail.com>
2023-09-24 22:45:59 +0530

    clone  an array

--- problemSolving.js/array_based_problem.js
+++ problemSolving.js/array_based_problem.js
@@ -7,17 +7,33 @@
 // const newArr=cloneArr([1,2,3,4,5,6])
 // console.log(newArr);
 
-// step 2
-function clone(arr){
-    var newarr =[]
-    arr.forEach((element )=> {
-        newarr.push(element);
+// method  2
+// function clone(arr){
+//     var newarr =[]
+//     arr.forEach((element )=> {
+//         newarr.push(element);
         
-    });
-    return newarr;
+//     });
+//     return newarr;
+// }
+// var sdfgh =[1,2,3,4]
+// var newArr=clone(sdfgh)
+// newArr.pop()
+// console.log(newArr)
+
+// methdo 3
+function clone(arr){
+    var newArr= arr.map((ele)=>{
+        return ele
+    })
+    return newArr
+
 }
-var sdfgh =[1,2,3,4]
-var newArr=clone(sdfgh)
-newArr.pop()
-console.log(newArr)
+var arr =clone([1,2,3,4])
+console.log(arr)
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++++
+// problem 2:=> write a javscript  function to get the first
+// element of an Array,passing a parameter "n" will return the first "n" element
+//  of the array
 
