4924d90efa8d2c6485551db36e201528f442b3f0
indrajeet790 <indrajeetkumarnayak3@gmail.com>
2023-09-24 22:08:24 +0530

    mixed array of string and number find number and print number sum

--- problemSolving.js/loop_array.js
+++ problemSolving.js/loop_array.js
@@ -1,5 +1,5 @@
 //  add array element and print sum
-
+/*
 var arr =[1,2,3,4,5,6,7]
 var sum=0
 
@@ -8,3 +8,15 @@ arr.forEach((ele)=>{
 
 })
 console.log(sum)
+*/
+
+// 
+var arr =["hehehehedfghj",123,"dfgh",1234654,"lkjhgf"]
+var sum =0; 
+arr.forEach((ele)=>{
+    if(typeof ele === "number"){
+        sum = sum+ele;
+    }
+ })
+ console.log(sum);
+ 
