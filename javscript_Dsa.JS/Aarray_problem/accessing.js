b5b63285558fad3f8eb72b59389d1dab59445fdd
indrajeet790 <indrajeetkumarnayak3@gmail.com>
2023-09-27 22:51:01 +0530

    trasversing element

---
+++ javscript_Dsa.JS/Aarray_problem/accessing.js
@@ -0,0 +1,6 @@
+//  accessing or trasversing element 
+let data =[0,45,2,54,23,7,78,67];
+for(let i=0; i<data.length; i++){
+    console.log(`Array ${i} is ${data[i]} `)
+}
+console.log(data[0])
--- problemSolving.js/array_problem.js
+++ problemSolving.js/array_problem.js
@@ -24,7 +24,7 @@
 // ########################################
 // problem 1: find second largest number fom a array
 
-
+/*
 function secondLagest(arr){
     let largest= -1;
     let secondLagest=-1
@@ -41,3 +41,25 @@ function secondLagest(arr){
 }
 console.log(secondLagest([12,34,23,45,65]))
 
+*/
+
+// problem:2 :=> rotate an array by
+function rotateArray(arr,k){
+    if(size > k){
+        k =k%size;
+    }
+    reverse(nums,0,nums.length -1)
+      reverse(nums,0,k -1)
+        reverse(nums,k,nums.length -1)
+        return nums;
+}
+function reverse(nums,left,right){
+    while(left < right){
+        const temp = nums[left];
+        nums[left++] =nums[right];
+        nums[right--] = temp;
+
+    }
+}
+
+console.log(rotateArray([1,2,3,4,5,6,7],3))
